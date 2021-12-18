47f98e2aa7ad3f5488a5bddb887f49c9a7f8ee31
tfs-apps-one <furu12080205@gmail.com>
2021-12-19 07:33:03 +0900

    DB設計

--- app/src/main/java/tfsapps/lightblink/MainActivity.java
+++ app/src/main/java/tfsapps/lightblink/MainActivity.java
@@ -2,14 +2,164 @@ package tfsapps.lightblink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//DB関連
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //  DB関連
+    private MyOpenHelper helper;        //DBアクセス
+    private int db_isopen = 0;          //DB使用したか
+    private int db_interval = 0;        //DB点滅間隔
+    private int db_brightness = 0;      //DB輝度調整
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         /* テストコード */
+    }
+    /* **************************************************
+        各種OS上の動作定義
+    ****************************************************/
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //DBのロード
+        /* データベース */
+        helper = new MyOpenHelper(this);
+        AppDBInitRoad();
+        screen_display();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        //  DB更新
+        AppDBUpdated();
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        //  DB更新
+        AppDBUpdated();
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        //  DB更新
+        AppDBUpdated();
+
+        //カメラ
+        /*
+        if (mCameraManager != null) {
+            mCameraManager = null;
+        }
+         */
+    }
+
+    /* **************************************************
+        表示処理
+    ****************************************************/
+    public void screen_display(){
+
+
+    }
+
+
+    /* **************************************************
+        DB初期ロードおよび設定
+    ****************************************************/
+    public void AppDBInitRoad() {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        StringBuilder sql = new StringBuilder();
+        sql.append(" SELECT");
+        sql.append(" isopen");
+        sql.append(" ,interval");
+        sql.append(" ,brightness");
+        sql.append(" FROM appinfo;");
+        try {
+            Cursor cursor = db.rawQuery(sql.toString(), null);
+            //TextViewに表示
+            StringBuilder text = new StringBuilder();
+            if (cursor.moveToNext()) {
+                db_isopen = cursor.getInt(0);
+                db_interval = cursor.getInt(1);
+                db_brightness = cursor.getInt(2);
+            }
+        } finally {
+            db.close();
+        }
+
+        db = helper.getWritableDatabase();
+        if (db_isopen == 0) {
+            long ret;
+            /* 新規レコード追加 */
+            ContentValues insertValues = new ContentValues();
+            insertValues.put("isopen", 1);
+            insertValues.put("interval", 1);
+            insertValues.put("brightness", 1);
+            insertValues.put("data1", 0);
+            insertValues.put("data2", 0);
+            insertValues.put("data3", 0);
+            insertValues.put("data4", 0);
+            insertValues.put("data5", 0);
+            insertValues.put("data6", 0);
+            insertValues.put("data7", 0);
+            insertValues.put("data8", 0);
+            insertValues.put("data9", 0);
+            insertValues.put("data10", 0);
+            try {
+                ret = db.insert("appinfo", null, insertValues);
+            } finally {
+                db.close();
+            }
+            db_isopen = 1;
+            db_interval = 1;
+            db_brightness = 1;
+
+            if (ret == -1) {
+                Toast.makeText(this, "DataBase Create.... ERROR", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "DataBase Create.... OK", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Data Loading...  interval:" + db_interval, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* **************************************************
+        DB更新
+    ****************************************************/
+    public void AppDBUpdated() {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues insertValues = new ContentValues();
+        insertValues.put("isopen", db_isopen);
+        insertValues.put("interval", db_interval);
+        insertValues.put("brightness", db_brightness);
+        int ret;
+        try {
+            ret = db.update("appinfo", insertValues, null, null);
+        } finally {
+            db.close();
+        }
+        if (ret == -1) {
+            Toast.makeText(this, "Saving.... ERROR ", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Saving.... OK "+ "op=0:"+db_isopen+" interval=1:"+db_interval+" brightness=2:"+db_brightness, Toast.LENGTH_SHORT).show();
+        }
     }
 }
