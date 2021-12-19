a1578740869889fc226179f1a51d2db0424b8eda
tfs-apps-one <furu12080205@gmail.com>
2021-12-19 16:07:10 +0900

    描画処理とボタンイベント処理の実装

--- app/src/main/java/tfsapps/lightblink/MainActivity.java
+++ app/src/main/java/tfsapps/lightblink/MainActivity.java
@@ -9,7 +9,19 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+//国設定
+import java.util.Locale;
+
+//スレッド関連
+import static java.lang.Thread.sleep;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +31,30 @@ public class MainActivity extends AppCompatActivity {
     private int db_interval = 0;        //DB点滅間隔
     private int db_brightness = 0;      //DB輝度調整
 
+    //  国設定
+    private Locale _local;
+    private String _language;
+    private String _country;
+
+    private ImageButton img_onoff;      //ライトON/OFFボタン
+    private ImageButton img_blink;      //点滅ボタン
+    private ImageButton img_brightness; //輝度調整ボタン
+    private SeekBar seek_blinkinterval; //点滅間隔
+    private SeekBar seek_brightness;    //輝度調整
+    private boolean isStart = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* テストコード */
+        //  国設定
+        _local = Locale.getDefault();
+        _language = _local.getLanguage();
+        _country = _local.getCountry();
+
+        //  シークバーの選択
+        seekSelect();
     }
     /* **************************************************
         各種OS上の動作定義
@@ -76,9 +105,113 @@ public class MainActivity extends AppCompatActivity {
     ****************************************************/
     public void screen_display(){
 
+        /* SEEK */
+        if (seek_blinkinterval == null) {
+            seek_blinkinterval = (SeekBar) findViewById(R.id.seek_blink);
+        }
+        seek_blinkinterval.setProgress(db_interval);
+
+        if (seek_brightness == null) {
+            seek_brightness = (SeekBar) findViewById(R.id.seek_brightness);
+        }
+        seek_brightness.setProgress(db_brightness);
+
+        /* IMAGE BUTTON */
+        if (img_onoff == null){
+            img_onoff = (ImageButton) findViewById(R.id.btn_img_onoff);
+        }
+        if (img_blink == null){
+            img_blink = (ImageButton) findViewById(R.id.btn_img_blink);
+        }
+        if (img_brightness == null){
+            img_brightness = (ImageButton) findViewById(R.id.btn_img_brightness);
+        }
+
+        /* ON時 */
+        if (isStart){
+            img_onoff.setImageResource(R.drawable.on_2);
+            img_blink.setImageResource(R.drawable.blink1_off);
+            img_brightness.setImageResource(R.drawable.bright1_off);
+        }
+        /* OFF時 */
+        else {
+            img_onoff.setImageResource(R.drawable.off_2);
+            img_blink.setImageResource(R.drawable.blink1_on);
+            img_brightness.setImageResource(R.drawable.bright1_on);
+        }
+
+        /* TEXT表示 */
+        TextView text_volume1 = (TextView)findViewById(R.id.text_blink);
+        text_volume1.setText(""+db_interval);
+        TextView text_volume2 = (TextView)findViewById(R.id.text_brightness);
+        text_volume2.setText(""+db_brightness);
 
     }
+    /* **************************************************
+        ライトスタート　ボタン処理
+    ****************************************************/
+    public void onStartStop(View view){
 
+        if (isStart){
+            isStart = false;
+        }
+        else{
+            isStart = true;
+        }
+
+        screen_display();
+    }
+
+    /* **************************************************
+        シークバー　選択時の処理
+    ****************************************************/
+    public void seekSelect(){
+        //  点滅間隔
+        seek_blinkinterval = (SeekBar)findViewById(R.id.seek_blink);
+        seek_blinkinterval.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    //ツマミをドラッグした時
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                        if (isStart == false){
+                            db_interval = seekBar.getProgress();
+                        }
+                        screen_display();
+                    }
+                    //ツマミに触れた時
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+                    //ツマミを離した時
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                }
+        );
+
+        //  輝度調整
+        seek_brightness = (SeekBar)findViewById(R.id.seek_brightness);
+        seek_brightness.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    //ツマミをドラッグした時
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                        if (isStart == false){
+                            db_brightness = seekBar.getProgress();
+                        }
+                        screen_display();
+                    }
+                    //ツマミに触れた時
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+                    //ツマミを離した時
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                }
+        );
+    }
 
     /* **************************************************
         DB初期ロードおよび設定
